c3fc1cbbd4ec0baeb056898851c0de8e923b4d9b
Peter Lorenz <0664j35t3r@users.noreply.github.com>
2015-10-09 18:18:18 +0200

    Create kubuntu_install.sh

---
+++ kubuntu_install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+apt-get update
+sudo apt-get install thunderbird -y
+sudo apt-get install kubuntu-restricted-extras -y
+sudo apt-get install chromium-browser -y
+sudo apt-get install terminator -y
+sudo apt-get install yakuake -y
+
+
+sudo add-apt-repository ppa:linrunner/tlp -y
+sudo apt-get update
+sudo apt-get install tlp tlp-rdw -y
+sudo tlp start
+
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install -y sublime-text-installer
